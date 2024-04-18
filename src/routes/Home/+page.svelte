1db9c24de4941a1467d36b1b633f68db95bb1f0f
BionicHawk <angel.manzo.rosas@gmail.com>
2024-04-18 13:30:45 -0600

    Se configuro el fondo de pantalla para que la imagen siga el cursor del raton y se est[a preparando la página de Home o Inicio

---
+++ src/routes/Home/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import AppHeader from '../components/Globals/AppHeader.svelte';
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+
+    function GoToLogin(event: MouseEvent): void {
+        if (event.button === 0) {
+            window.open('/', '_self');
+        }
+    }
+</script>
+
+<AppHeader onClickHeaderElement={GoToLogin} />
+
+<main>
+
+</main>
+
+<style>
+    main {
+        height: max-content;
+        margin: 70px 0 0 0;
+    }
+</style>
